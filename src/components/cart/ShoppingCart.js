e466c4507a307fcdc20bd0edc3344b45f5793217
Giang Vo <giangvo2511@gmail.com>
2021-07-22 20:07:01 +0700

    update connect state

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -1,45 +1,44 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCart } from '../../actions/cartAction';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCart } from "../../actions/cartAction";
 
-import SectionHero from '../common/SectionHero';
-import SummaryCart from './SummaryCart';
-import TableCart from './TableCart';
-
+import SectionHero from "../common/SectionHero";
+import SummaryCart from "./SummaryCart";
+import TableCart from "./TableCart";
 
 const ShoppingCart = () => {
-    const dispatch = useDispatch();
-    let cartLocal = JSON.parse(localStorage.getItem("carts")) || [];
-    const carts = useSelector(state => state.carts.list);
+  const dispatch = useDispatch();
+  let cartLocal = JSON.parse(localStorage.getItem("carts")) || [];
+  const carts = useSelector((state) => state.carts.list);
 
-    useEffect(() => {
-        dispatch(setCart(cartLocal));
-    },[dispatch]);
-    return (
-        <React.Fragment>
-            <SectionHero 
-                title="Shopping cart" 
-                desc="description"
-                bg="../img/hero_1.jpeg"
-            />
-            <section className="section section--shopping-cart">
-                <div className="container">
-                    <div className="shopping-cart">
-                        <div className="shopping-cart__left">
-                            <div className="flex flex-middle">
-                                <h2 className="h2">Your cart</h2>
-                                <div className="ml1rem">{carts.length || 0} item(s)</div>
-                            </div>
-                            <TableCart carts={carts}/>
-                        </div>
-                        <div className="shopping-cart__right">
-                            <h2 className="h2">Summary</h2>
-                            <SummaryCart carts={carts}/>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </React.Fragment>
-    );
-}
+  useEffect(() => {
+    dispatch(setCart(cartLocal));
+  }, [dispatch]);
+  return (
+    <React.Fragment>
+      <SectionHero
+        title="Shopping cart"
+        desc="description"
+        bg="../img/hero_1.jpeg"
+      />
+      <section className="section section--shopping-cart">
+        <div className="container">
+          <div className="shopping-cart">
+            <div className="shopping-cart__left">
+              <div className="flex flex-middle">
+                <h2 className="h2">Your cart</h2>
+                <div className="ml1rem">{carts.length || 0} item(s)</div>
+              </div>
+              <TableCart carts={carts} />
+            </div>
+            <div className="shopping-cart__right">
+              <h2 className="h2">Summary</h2>
+              <SummaryCart carts={carts} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
 export default ShoppingCart;
--- src/components/cart/SummaryCart.js
+++ src/components/cart/SummaryCart.js
@@ -1,56 +1,67 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 
+const Component = () => {
+  const history = useHistory();
+  const carts = useSelector((state) => state.carts.list);
+  let shippingFee = 0;
+  let subtotal = 0;
+  if (carts && carts.length > 0) {
+    carts.forEach((cart) => {
+      console.log(parseInt(cart.price), parseInt(cart.quantity));
+      subtotal += parseInt(cart.price) * parseInt(cart.quantity);
+      console.log("subtotal", subtotal);
+    });
+  }
 
-const SummaryCart = ({carts}) => {
-    
-    const history = useHistory();
-
-    console.log('carts summary', carts);
-    let shippingFee = 0;
-    let subtotal = 0;
-    if(carts && carts.length > 0) {
-        carts.forEach(cart => {
-            console.log(parseInt(cart.price), parseInt(cart.quantity));
-            subtotal += (parseInt(cart.price) * parseInt(cart.quantity));
-            console.log('subtotal',subtotal);
-        })
-    } 
-    
-    return (
-        <React.Fragment>
-            <div className="summary mt1rem">
-                <div className="summary__body">
-                    <div className="summary__row flex flex-between">
-                        <div className="summary__label text-gray">Subtotal</div>
-                        <div className="summary__number">
-                            <u>đ</u>
-                            <span>{subtotal}</span>
-                        </div>
-                    </div>
-                    <div className="summary__row flex flex-between mt1rem">
-                        <div className="summary__label text-gray">Shipping fee</div>
-                        <div className="summary__number">
-                            <u>đ</u>
-                            <span>{shippingFee}</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="summary__footer">
-                    <div className="summary__row  flex flex-between flex-middle">
-                        <div className="summary__label"><span>Total</span></div>
-                        <div className="h3 b summary__number">
-                            <u>đ</u>
-                            <span>{subtotal + shippingFee}</span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <React.Fragment>
+      <div className="summary mt1rem">
+        <div className="summary__body">
+          <div className="summary__row flex flex-between">
+            <div className="summary__label text-gray">Subtotal</div>
+            <div className="summary__number">
+              <u>đ</u>
+              <span>{subtotal}</span>
             </div>
-            <button type="button" 
-                onClick={() => history.push('./checkout')}
-                className="btn btn--primary btn--block mt1rem">checkout</button>
-        </React.Fragment>
-    );
-}
+          </div>
+          <div className="summary__row flex flex-between mt1rem">
+            <div className="summary__label text-gray">Shipping fee</div>
+            <div className="summary__number">
+              <u>đ</u>
+              <span>{shippingFee}</span>
+            </div>
+          </div>
+        </div>
+        <div className="summary__footer">
+          <div className="summary__row  flex flex-between flex-middle">
+            <div className="summary__label">
+              <span>Total</span>
+            </div>
+            <div className="h3 b summary__number">
+              <u>đ</u>
+              <span>{subtotal + shippingFee}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        type="button"
+        onClick={() => history.push("./checkout")}
+        className="btn btn--primary btn--block mt1rem"
+      >
+        checkout
+      </button>
+    </React.Fragment>
+  );
+};
+
+const SummaryCart = connect((state) => ({
+  carts: state.carts,
+}))(Component);
 
 export default SummaryCart;
+
+// export default SummaryCart;
